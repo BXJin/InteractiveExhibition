e60027778e78c62e4baee852820f23fbca6e5c7c
BXJin <bsung472@gmail.com>
2026-03-28 20:02:13 +0900

    Unreal Websocket 통신 클래스 추가

---
+++ Client-Unreal/ExhibitionClient/Source/ExhibitionClient/Private/Realtime/ExhibitionRealtimeSubsystem.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Realtime/ExhibitionRealtimeSubsystem.h"
+
+void UExhibitionRealtimeSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+}
+
+void UExhibitionRealtimeSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+}
